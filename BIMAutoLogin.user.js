6755078b9ed90c9fb625b582f3360689b7888971
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 14:50:38 -0800

    Add files via upload

---
+++ BIMAutoLogin.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BIM Auto-Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://bigideaslearning.magicsw.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=magicsw.com
+// @include      https://bigideaslearning.magicsw.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById("class-login").click();
+})();
