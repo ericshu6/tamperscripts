fe26064e4c99ea21916b827ad09bd4de9b60d785
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 15:38:11 -0800

    Update BimAlg1.user.js

--- BimAlg1.user.js
+++ BimAlg1.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  When library page is loaded, go to Algebra 1
-// @author       You
+// @author       Eric S.
 // @match        https://bigideaslearning.magicsw.com/mylibrary.htm
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=magicsw.com
 // @grant        none
