f04e651dc314abe24954cdebc071d291a4868d5e
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 15:38:43 -0800

    Update BIMAutoLogin.user.js

--- BIMAutoLogin.user.js
+++ BIMAutoLogin.user.js
@@ -2,7 +2,7 @@
 // @name         BIM Auto-Login
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  try to take over the world!
+// @description  Automatically logs you into Big Ideas Math library
 // @author       Eric S.
 // @match        https://bigideaslearning.magicsw.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=magicsw.com
