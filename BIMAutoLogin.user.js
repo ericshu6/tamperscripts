56e64e063469e8ff15661dc5c31bb47a39dafafe
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 15:29:47 -0800

    what kind of

--- BIMAutoLogin.user.js
+++ BIMAutoLogin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BIM Auto-Login
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://bigideaslearning.magicsw.com/*
