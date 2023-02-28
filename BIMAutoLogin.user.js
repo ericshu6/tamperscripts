c2ee56f219a3d0191d1deb3a9353d12e6473abc2
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 15:34:19 -0800

    Update BIMAutoLogin.user.js

--- BIMAutoLogin.user.js
+++ BIMAutoLogin.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
-// @author       You
+// @author       Eric S.
 // @match        https://bigideaslearning.magicsw.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=magicsw.com
 // @include      https://bigideaslearning.magicsw.com
