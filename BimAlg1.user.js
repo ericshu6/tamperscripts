23f961ce2072fa16c2045ed32610c5acad8bdf96
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-02-28 15:37:47 -0800

    Add files via upload

---
+++ BimAlg1.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BIM Alg. 1
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  When library page is loaded, go to Algebra 1
+// @author       You
+// @match        https://bigideaslearning.magicsw.com/mylibrary.htm
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=magicsw.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location.href = "/ebookreader/launchbook.htm?id=1433&amp;userType=RUNPTV9VU0VS&amp;_=1677624505554";
+})();
