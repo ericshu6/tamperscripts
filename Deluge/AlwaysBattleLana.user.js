3ae94925a63640740e71ae3eb8ee496aaebb12f1
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-03-02 22:45:56 -0800

    Create AlwaysBattleLana.user.js

---
+++ Deluge/AlwaysBattleLana.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Always Battle Lana
+// @namespace    https://github.com/GameBoyYeet/tamperscripts/
+// @version      0.1
+// @description  Automate Lana's Trauma
+// @author       Eric S.
+// @match        https://www.delugerpg.com/trainers*
+// @icon         https://i.dstatic.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location.href = "https://www.delugerpg.com/battle/trainer/703";
+})();
