9c4ddf731c0281bdcdb7b1869c2cf1ebf5cc505c
Eric Shu <45149431+GameBoyYeet@users.noreply.github.com>
2023-03-28 10:05:13 -0700

    Create ClassLink.user.js

---
+++ ClassLink.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CVUSD ClassLink
+// @namespace    http://github.com/gameboyyeet/tamperscripts
+// @version      0.1
+// @description  Redirects to ClassLink SSO
+// @author       Eric S.
+// @match        https://apps.explorelearning.com/account/gizmos/login/student*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=explorelearning.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location.href = "https://launchpad.classlink.com/ltisso/1870659";
+})();
